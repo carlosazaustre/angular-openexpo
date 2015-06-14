f22ce7e1bf13c49a1a4be7e667d3c5b8728e63c7
Carlos Azaustre <cazaustre@gmail.com>
2015-06-14 20:10:51 +0200

    add components

---
+++ app/app.js
@@ -0,0 +1,31 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('directorio', ['ngRoute'])
+    .config(directorioConfig);
+
+  directorioConfig.$inject = ['$routeProvider', '$locationProvider'];
+
+  function directorioConfig ($routeProvider, $locationProvider) {
+
+    $locationProvider.html5Mode(true);
+
+    $routeProvider
+      .when('/', {
+        templateUrl : 'app/components/listado/listado.html',
+        controller  : 'ListadoController',
+        controllerAs: 'listado'
+      })
+      .when('/empleado/:empladoId', {
+        templateUrl : 'app/components/empleado/empleado.html',
+        controller  : 'EmpleadoController',
+        controllerAs: 'empleado'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }
+
+
+})();
